ea59232ab4424b8d916b51ffe542d538290d3590
Roy Jung <phi4grv@gmail.com>
2016-03-06 15:05:15 +1100

    Disable devise-token-auth oauth authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   health_check_routes
 
   namespace :v1 do
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     resources :clients, only: :create
     post 'tokens', to: proc { |env| TokenEndpoint.new.call(env) }
   end
