6e6d45f09b800a3bd4d9ebb3d0f8bf232eeec65a
Roy Jung <phi4grv@gmail.com>
2016-03-26 00:23:13 +1100

    Add redirect_uris for example client

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,7 +48,10 @@ end
 unless Client.find_by user_account: internal_account, name: Client::PARTI_AUTH_EXAMPE_CLIENT_NAME
   client = Client.create!(
     name: Client::PARTI_AUTH_EXAMPE_CLIENT_NAME,
-    redirect_uris: 'http://localhost:5000/parti/callback',
+    redirect_uris: [
+      'http://localhost:5000/auth/parti/callback',
+      'http://localhost:5000/parti_callback'
+    ],
     user_account: internal_account,
   )
   client.update(
