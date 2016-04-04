21eb0c6ba5ea16e999582950275bb840e1354b03
Roy Jung <phi4grv@gmail.com>
2016-04-04 14:29:49 +1000

    Back to use build in docker-compose-test.yml

--- deploy/build-docker-image.bash
+++ deploy/build-docker-image.bash
@@ -17,6 +17,4 @@ DOCKER_REPO=partixyz/auth-api
 APP_VERSION=${APP_VERSION:-$( git describe --tags --long )}
 
 docker build -t ${DOCKER_REPO}:${APP_VERSION} ${SCRIPT_DIR}/..
-docker tag ${DOCKER_REPO}:${APP_VERSION} $DOCKER_REPO:current
-
-
+# docker tag ${DOCKER_REPO}:${APP_VERSION} $DOCKER_REPO:current
