0e2c59ba1465436a8b0bd5a98b883c436b98e628
Roy Jung <phi4grv@gmail.com>
2016-04-15 15:37:29 +1000

    Change database cleaner strategy

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,14 +1,7 @@
 RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-    load "#{Rails.root}/db/seeds.rb"
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
     load "#{Rails.root}/db/seeds.rb"
   end
 end
